fb4b5d9dab9c4af5d7d2e09b9b7fb4ae8dceece8
Vlad-B <bogdanionutvlad@outlook.com>
2021-11-05 16:10:08 +0000

    Configure index.js to allow bot to use voice

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 const { Client, Intents, Collection } = require('discord.js')
 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config()
 const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_VOICE_STATES
+	]
 })
 
 client.commands = new Collection();
@@ -22,18 +23,22 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-    console.log('Bot is running')
+	console.log('Bot is running')
 })
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
 	if (!interaction.isCommand()) return;
 
 	const command = client.commands.get(interaction.commandName);
+	const args = [];
+	interaction.options.data.map((o) => {
+		args.push(o.value)
+	})
 
 	if (!command) return;
-
+	
 	try {
-		await command.execute(interaction);
+		await command.execute(interaction, args);
 	} catch (error) {
 		console.error(error);
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
