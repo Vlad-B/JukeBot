ca8d9e27f34e9830dfc400772143dfa149e1205d
Vlad-B <bogdanionutvlad@outlook.com>
2021-11-05 16:09:21 +0000

    Add play.js command and check for permissions

---
+++ commands/play.js
@@ -0,0 +1,32 @@
+const { Permissions } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const ytdl = require('ytdl-core');
+const yTsearch = require('yt-search')
+const {
+	AudioPlayerStatus,
+	StreamType,
+	createAudioPlayer,
+	createAudioResource,
+	joinVoiceChannel,
+} = require('@discordjs/voice');
+
+const dotenv = require('dotenv');
+dotenv.config()
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('Plays from YouTube.')
+		.addStringOption(option => option
+			.setName('url')
+			.setDescription('Play a song from YouTube')
+			.setRequired(true)),
+
+	async execute(interaction, args) {
+		args = interaction.options.getString('url');
+		const reqPerms = [ Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK ];
+		
+		if(!interaction.member.permissions.has(reqPerms)) await interaction.reply("You don't have the required permissions.");
+  	}
+}
+   
