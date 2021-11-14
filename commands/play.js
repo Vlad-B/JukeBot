6329235f6142bcd738556954f094b89577cb3be9
Vlad-B <bogdanionutvlad@outlook.com>
2021-11-14 10:18:09 +0000

    Set up play.js for dispatcher file

--- commands/play.js
+++ commands/play.js
@@ -10,28 +10,23 @@ const {
 	joinVoiceChannel,
 } = require('@discordjs/voice');
 
-// const dotenv = require('dotenv');
-// dotenv.config()
-// const guildId = process.env.GUILD_ID;
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
 		.setDescription('Plays from YouTube.')
 		.addStringOption(option => option
-			.setName('url')
-			.setDescription('Play a song from YouTube')
+			.setName('title')
+			.setDescription('Song title.')
 			.setRequired(true)),
-
+	player: createAudioPlayer(),
 	async execute(interaction, args) {
-		args = interaction.options.getString('url');
-		const voiceChannel = interaction.member.voice.channel
+		args = interaction.options.getString('title');
+		const voiceChannel = interaction.member.voice.channel;
 		const reqPerms = [ Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK ];
 		
-		if(!interaction.member.permissions.has(reqPerms)) await interaction.reply("You don't have the required permissions.");
+		if(!interaction.member.permissions.has(reqPerms)) return interaction.reply("You don't have the required permissions.");
 		if(!voiceChannel) return interaction.reply('You need to be in a voice channel to execute this command!');
 
-		// console.log(voiceChannel)
 		const connection = joinVoiceChannel({
 			channelId: voiceChannel.id,
 			guildId: voiceChannel.guildId,
@@ -41,23 +36,22 @@ module.exports = {
 
 		const videoFinder = async (query) => {
 			const videoResults = await yTsearch(query)
-			return (videoResults.videos.length > 1) ? videoResults.videos[0] : null
+			return (videoResults.videos.length > 1) ? videoResults.videos[0] : null;
 		}
-		const video = await videoFinder(args)
-		const player = createAudioPlayer();
+		const video = await videoFinder(args);
 		
 		if(video) {
 			const stream = ytdl(video.url, { filter: 'audioonly' });
 			const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-		
-			player.play(resource);
-			connection.subscribe(player);
+			
+			connection.subscribe(this.player);
+			this.player.play(resource);
 			
 			await interaction.reply(`:call_me: Now playing *** ${video.title} ***`)
 		} else {
 			await interaction.reply('No video results found.')
 		}
-		player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+
+		this.player.on(AudioPlayerStatus.Idle, () => connection.destroy());
   	}
 }
-   
