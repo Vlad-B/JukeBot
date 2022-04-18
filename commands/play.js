60411a66d09bdbbf178dfffec35d475f70181a69
Bogdan Vlad <hello@bogdanvlad.com>
2022-04-18 17:06:47 +0100

    Change streaming dependency to play-dl
    ytdl kept breaking the app with its new version

--- commands/play.js
+++ commands/play.js
@@ -1,67 +1,81 @@
-const { Permissions } = require('discord.js')
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const ytdl = require('ytdl-core');
-const yTsearch = require('yt-search')
+const { Permissions } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const play = require("play-dl");
+const yTsearch = require("yt-search");
 const {
-	AudioPlayerStatus,
-	StreamType,
-	createAudioPlayer,
-	createAudioResource,
-	joinVoiceChannel,
-} = require('@discordjs/voice');
+    AudioPlayerStatus,
+    createAudioPlayer,
+    createAudioResource,
+    joinVoiceChannel,
+} = require("@discordjs/voice");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('play')
-		.setDescription('Plays from YouTube.')
-		.addStringOption(option => option
-			.setName('title')
-			.setDescription('Song title.')
-			.setRequired(true)),
+    data: new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Plays from YouTube.")
+        .addStringOption((option) =>
+            option
+                .setName("title")
+                .setDescription("Song title.")
+                .setRequired(true)
+        ),
 
-	player: createAudioPlayer(),
-	
-	voiceChannel: (interaction) => {
-		const voiceChannel = interaction.member.voice.channel;
-		return voiceChannel;
-	},
-	
-	checkPerms: (interaction, channel) => {
-		const reqPerms = [ Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK ];
-		
-		if(!interaction.member.permissions.has(reqPerms)) return interaction.reply("You don't have the required permissions.");
-		if(!channel) return interaction.reply('You need to be in a voice channel to execute this command!');
-	},
-	
-	async execute(interaction, args) {
-		args = interaction.options.getString('title');
-		this.checkPerms(interaction, this.voiceChannel(interaction))
+    player: createAudioPlayer(),
 
-		const connection = joinVoiceChannel({
-			channelId: this.voiceChannel(interaction).id,
-			guildId: this.voiceChannel(interaction).guildId,
-			adapterCreator: this.voiceChannel(interaction).guild.voiceAdapterCreator,
-			selfDeaf: false
-		});
+    voiceChannel: (interaction) => {
+        const voiceChannel = interaction.member.voice.channel;
+        return voiceChannel;
+    },
 
-		const videoFinder = async (query) => {
-			const videoResults = await yTsearch(query)
-			return (videoResults.videos.length > 1) ? videoResults.videos[0] : null;
-		}
-		const video = await videoFinder(args);
-		
-		if(video) {
-			const stream = ytdl(video.url, { filter: 'audioonly' });
-			const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-			
-			connection.subscribe(this.player);
-			this.player.play(resource);
-			
-			await interaction.reply(`:call_me: Now playing *** ${video.title} ***`)
-		} else {
-			await interaction.reply('No video results found.')
-		}
+    checkPerms: (interaction, channel) => {
+        const reqPerms = [Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK];
 
-		this.player.on(AudioPlayerStatus.Idle, () => connection.destroy());
-  	}
-}
+        if (!interaction.member.permissions.has(reqPerms))
+            return interaction.reply(
+                "You don't have the required permissions."
+            );
+        if (!channel)
+            return interaction.reply(
+                "You need to be in a voice channel to execute this command!"
+            );
+    },
+
+    async execute(interaction, args) {
+        args = interaction.options.getString("title");
+        this.checkPerms(interaction, this.voiceChannel(interaction));
+
+        const connection = joinVoiceChannel({
+            channelId: this.voiceChannel(interaction).id,
+            guildId: this.voiceChannel(interaction).guildId,
+            adapterCreator:
+                this.voiceChannel(interaction).guild.voiceAdapterCreator,
+            selfDeaf: false,
+        });
+
+        const videoFinder = async (query) => {
+            const videoResults = await yTsearch(query);
+            return videoResults.videos.length > 1
+                ? videoResults.videos[0]
+                : null;
+        };
+        const video = await videoFinder(args);
+
+        if (video) {
+            const stream = await play.stream(video.url);
+            const resource = createAudioResource(stream.stream, {
+                inputType: stream.type,
+            });
+
+            connection.subscribe(this.player);
+            this.player.play(resource);
+
+            await interaction.reply(
+                `:call_me: Now playing *** ${video.title} ***`
+            );
+        } else {
+            await interaction.reply("No video results found.");
+        }
+
+        this.player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+    },
+};
